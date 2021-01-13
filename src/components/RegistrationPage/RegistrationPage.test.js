b148702ab00ab97d429e7789d6559880928746aa
Logan Thirion <thirionlogan@gmail.com>
2021-01-13 15:08:05 -0600

    add memory router

--- src/components/RegistrationPage/RegistrationPage.test.js
+++ src/components/RegistrationPage/RegistrationPage.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import RegistrationPage from './RegistrationPage';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('RegistrationPage', () => {
   let component;
   beforeEach(() => {
-    component = mount(<RegistrationPage />);
+    component = mount(
+      <MemoryRouter>
+        <RegistrationPage />
+      </MemoryRouter>
+    );
   });
   it('should render', () => {
     expect(component.exists('div')).toBe(true);
