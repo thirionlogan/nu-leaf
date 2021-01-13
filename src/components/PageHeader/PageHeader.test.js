0f66e0ea84fe9684eb5e0fadb50631dd7f950315
Logan Thirion <thirionlogan@gmail.com>
2021-01-13 12:23:42 -0600

    add memory router to tests

--- src/components/PageHeader/PageHeader.test.js
+++ src/components/PageHeader/PageHeader.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import PageHeader from './PageHeader';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('PageHeader', () => {
   let component;
   beforeEach(() => {
-    component = mount(<PageHeader />);
+    component = mount(
+      <MemoryRouter>
+        <PageHeader />
+      </MemoryRouter>
+    );
   });
   it('should render', () => {
     expect(component.exists('div')).toBe(true);
