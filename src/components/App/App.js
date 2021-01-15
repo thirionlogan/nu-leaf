bbb266e09642bfa0f51f59be4f185ba7fe18663c
Logan Thirion <thirionlogan@gmail.com>
2021-01-14 19:27:07 -0600

    #34 implement registration Page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LandingPage from '../LandingPage/LandingPage';
 import PageHeader from '../PageHeader/PageHeader';
 import RegistrationPage from '../RegistrationPage/RegistrationPage';
-import LoginPage from '../LoginPage/LoginPage'
+import LoginPage from '../LoginPage/LoginPage';
+import { registerUser } from '../../client/client';
 
 const theme = createMuiTheme({
   palette: {
@@ -26,7 +27,9 @@ function App() {
           <PageHeader />
           <Switch>
             <Route exact path='/' component={LandingPage} />
-            <Route path='/register' component={RegistrationPage} />
+            <Route path='/register'>
+              <RegistrationPage registerUser={registerUser} />
+            </Route>
             <Route path='/login' component={LoginPage} />
           </Switch>
         </div>
--- src/components/RegistrationPage/RegistrationPage.test.js
+++ src/components/RegistrationPage/RegistrationPage.test.js
@@ -2,17 +2,189 @@ import React from 'react';
 import { mount } from 'enzyme';
 import RegistrationPage from './RegistrationPage';
 import { MemoryRouter } from 'react-router-dom';
+import { mockRegisterUser } from '../../client/mockClient';
+import { Button } from '@material-ui/core';
 
 describe('RegistrationPage', () => {
   let component;
   beforeEach(() => {
     component = mount(
       <MemoryRouter>
-        <RegistrationPage />
+        <RegistrationPage
+          registerUser={async (params) => mockRegisterUser(params)}
+        />
       </MemoryRouter>
     );
   });
-  it('should render', () => {
-    expect(component.exists('div')).toBe(true);
+  const newUser = {
+    firstName: 'Bob',
+    lastName: 'Smith',
+    email: 'bobsmith@email.com',
+    password: 'password',
+  };
+  it('should register user', () => {
+    component
+      .find('#firstName')
+      .find('input')
+      .simulate('change', { target: { value: newUser.firstName } });
+
+    component
+      .find('#lastName')
+      .find('input')
+      .simulate('change', { target: { value: newUser.lastName } });
+
+    component
+      .find('#email')
+      .find('input')
+      .simulate('change', { target: { value: newUser.email } });
+
+    component
+      .find('#password')
+      .find('input')
+      .simulate('change', { target: { value: newUser.password } });
+    component
+      .find('#confirmPassword')
+      .find('input')
+      .simulate('change', { target: { value: newUser.password } });
+
+    component.find(Button).simulate('click');
+
+    expect(mockRegisterUser).toBeCalledWith({
+      ...newUser,
+      confirmPassword: newUser.password,
+    });
+  });
+  it('should require First Name', () => {
+    component
+      .find('#lastName')
+      .find('input')
+      .simulate('change', { target: { value: newUser.lastName } });
+
+    component
+      .find('#email')
+      .find('input')
+      .simulate('change', { target: { value: newUser.email } });
+
+    component
+      .find('#password')
+      .find('input')
+      .simulate('change', { target: { value: newUser.password } });
+    component
+      .find('#confirmPassword')
+      .find('input')
+      .simulate('change', { target: { value: newUser.password } });
+    expect(mockRegisterUser).not.toBeCalled();
+  });
+  it('should require Last Name', () => {
+    component
+      .find('#firstName')
+      .find('input')
+      .simulate('change', { target: { value: newUser.firstName } });
+
+    component
+      .find('#email')
+      .find('input')
+      .simulate('change', { target: { value: newUser.email } });
+
+    component
+      .find('#password')
+      .find('input')
+      .simulate('change', { target: { value: newUser.password } });
+    component
+      .find('#confirmPassword')
+      .find('input')
+      .simulate('change', { target: { value: newUser.password } });
+    expect(mockRegisterUser).not.toBeCalled();
+  });
+  it('should require Email', () => {
+    component
+      .find('#firstName')
+      .find('input')
+      .simulate('change', { target: { value: newUser.firstName } });
+
+    component
+      .find('#lastName')
+      .find('input')
+      .simulate('change', { target: { value: newUser.lastName } });
+
+    component
+      .find('#password')
+      .find('input')
+      .simulate('change', { target: { value: newUser.password } });
+    component
+      .find('#confirmPassword')
+      .find('input')
+      .simulate('change', { target: { value: newUser.password } });
+    expect(mockRegisterUser).not.toBeCalled();
+  });
+  it('should require Password', () => {
+    component
+      .find('#firstName')
+      .find('input')
+      .simulate('change', { target: { value: newUser.firstName } });
+
+    component
+      .find('#lastName')
+      .find('input')
+      .simulate('change', { target: { value: newUser.lastName } });
+
+    component
+      .find('#email')
+      .find('input')
+      .simulate('change', { target: { value: newUser.email } });
+
+    component
+      .find('#confirmPassword')
+      .find('input')
+      .simulate('change', { target: { value: newUser.password } });
+    expect(mockRegisterUser).not.toBeCalled();
+  });
+  it('should require Confirm Password', () => {
+    component
+      .find('#firstName')
+      .find('input')
+      .simulate('change', { target: { value: newUser.firstName } });
+
+    component
+      .find('#lastName')
+      .find('input')
+      .simulate('change', { target: { value: newUser.lastName } });
+
+    component
+      .find('#email')
+      .find('input')
+      .simulate('change', { target: { value: newUser.email } });
+
+    component
+      .find('#password')
+      .find('input')
+      .simulate('change', { target: { value: newUser.password } });
+    expect(mockRegisterUser).not.toBeCalled();
+  });
+  it('should require Confirm Password to be the same as Password', () => {
+    component
+      .find('#firstName')
+      .find('input')
+      .simulate('change', { target: { value: newUser.firstName } });
+
+    component
+      .find('#lastName')
+      .find('input')
+      .simulate('change', { target: { value: newUser.lastName } });
+
+    component
+      .find('#email')
+      .find('input')
+      .simulate('change', { target: { value: newUser.email } });
+
+    component
+      .find('#password')
+      .find('input')
+      .simulate('change', { target: { value: newUser.password } });
+    component
+      .find('#confirmPassword')
+      .find('input')
+      .simulate('change', { target: { value: 'different password' } });
+    expect(mockRegisterUser).not.toBeCalled();
   });
 });
