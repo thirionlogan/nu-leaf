010f568bc730fc9ac4e1e7b609eb608664c102a3
Logan Thirion <thirionlogan@gmail.com>
2021-01-14 19:27:07 -0600

    #34 implement registration Page

--- src/components/RegistrationPage/RegistrationPage.js
+++ src/components/RegistrationPage/RegistrationPage.js
@@ -1,16 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, Redirect } from 'react-router-dom';
 import Container from '@material-ui/core/Container';
 import Copyright from '../Copyright/Copyright';
 
@@ -34,11 +32,96 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function RegistrationPage() {
+export default function RegistrationPage({ registerUser }) {
   const classes = useStyles();
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [redirect, setRedirect] = useState(false);
+
+  const [firstNameError, setFirstNameError] = useState('');
+  const [lastNameError, setLastNameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [confirmPasswordError, setConfirmPasswordError] = useState('');
+
+  useEffect(() => {
+    setFirstNameError('');
+  }, [firstName]);
+  useEffect(() => {
+    setLastNameError('');
+  }, [lastName]);
+  useEffect(() => {
+    setEmailError('');
+  }, [email]);
+  useEffect(() => {
+    setPasswordError('');
+  }, [password]);
+  useEffect(() => {
+    setConfirmPasswordError('');
+  }, [confirmPassword]);
+
+  const handleChangeFirstName = (event) => {
+    setFirstName(event.target.value);
+  };
+  const handleChangeLastName = (event) => {
+    setLastName(event.target.value);
+  };
+  const handleChangeEmail = (event) => {
+    setEmail(event.target.value);
+  };
+  const handleChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
+  const handleChangeConfirmPassword = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!firstName.trim()) {
+      setFirstNameError('First Name is required');
+    }
+    if (!lastName.trim()) {
+      setLastNameError('Last Name is required');
+    }
+    if (!email.trim()) {
+      setEmailError('Email is required');
+    }
+    if (!password.trim()) {
+      setPasswordError('Password is required');
+    }
+    if (!confirmPassword.trim()) {
+      setConfirmPasswordError('Please confirm password');
+    }
+    if (!password === confirmPassword) {
+      setConfirmPasswordError('Must match password');
+    }
+
+    const noErrors =
+      firstName.trim() &&
+      lastName.trim() &&
+      email.trim() &&
+      password.trim() &&
+      confirmPassword.trim() &&
+      password === confirmPassword;
+
+    if (noErrors) {
+      registerUser({ firstName, lastName, email, password, confirmPassword })
+        .then((res) => setRedirect(true))
+        .catch(() => {
+          // TODO handle error
+        });
+    }
+  };
 
   return (
     <Container component='main' maxWidth='xs'>
+      {redirect ? <Redirect to='/login' /> : null}
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
@@ -46,7 +129,7 @@ export default function RegistrationPage() {
         <Typography component='h1' variant='h5'>
           Sign up
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className={classes.form} onSubmit={(e) => e.preventDefault()}>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
               <TextField
@@ -57,7 +140,11 @@ export default function RegistrationPage() {
                 fullWidth
                 id='firstName'
                 label='First Name'
+                value={firstName}
+                onChange={handleChangeFirstName}
                 autoFocus
+                helperText={firstNameError}
+                error={!!firstNameError}
               />
             </Grid>
             <Grid item xs={12} sm={6}>
@@ -69,6 +156,10 @@ export default function RegistrationPage() {
                 label='Last Name'
                 name='lastName'
                 autoComplete='lname'
+                value={lastName}
+                onChange={handleChangeLastName}
+                helperText={lastNameError}
+                error={!!lastNameError}
               />
             </Grid>
             <Grid item xs={12}>
@@ -80,6 +171,10 @@ export default function RegistrationPage() {
                 label='Email Address'
                 name='email'
                 autoComplete='email'
+                value={email}
+                onChange={handleChangeEmail}
+                helperText={emailError}
+                error={!!emailError}
               />
             </Grid>
             <Grid item xs={12}>
@@ -92,6 +187,10 @@ export default function RegistrationPage() {
                 type='password'
                 id='password'
                 autoComplete='current-password'
+                value={password}
+                onChange={handleChangePassword}
+                helperText={passwordError}
+                error={!!passwordError}
               />
             </Grid>
             <Grid item xs={12}>
@@ -99,17 +198,15 @@ export default function RegistrationPage() {
                 variant='outlined'
                 required
                 fullWidth
-                name='password'
+                name='confirmPassword'
                 label='Confirm Password'
                 type='password'
-                id='password'
+                id='confirmPassword'
                 autoComplete='current-password'
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value='allowExtraEmails' color='primary' />}
-                label='I want to receive inspiration, marketing promotions and updates via email.'
+                value={confirmPassword}
+                onChange={handleChangeConfirmPassword}
+                helperText={confirmPasswordError}
+                error={!!confirmPasswordError}
               />
             </Grid>
           </Grid>
@@ -119,6 +216,7 @@ export default function RegistrationPage() {
             variant='contained'
             color='primary'
             className={classes.submit}
+            onClick={handleSubmit}
           >
             Sign Up
           </Button>
