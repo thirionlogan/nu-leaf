888c44582a4c790f79e49af470b1268a533c5ad6
Logan Thirion <thirionlogan@gmail.com>
2021-01-15 12:43:38 -0600

    fix seed

--- server/data/seeds/002_seed_resolutions.js
+++ server/data/seeds/002_seed_resolutions.js
@@ -1,10 +1,15 @@
 exports.seed = (knex) => {
   return knex('resolution')
     .del()
-    .then(() => {
-      return knex('resolution').insert([
-        { user_id: 1, content: 'lose 15 lbs' },
-        { user_id: 1, content: 'actually fulfill resolutions this year' },
-      ]);
-    });
+    .then(() =>
+      knex
+        .select('id')
+        .from('user')
+        .then(([{ id }]) =>
+          knex('resolution').insert([
+            { user_id: id, content: 'Lose 15 lbs' },
+            { user_id: id, content: 'Actually fulfill resolutions this year' },
+          ])
+        )
+    );
 };
